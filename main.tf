530f4c45149980f8014c920ec85cfa142013a020
Kishore-08 <kishore.tngl@gmail,com>
2024-02-15 17:16:56 +0530

    initial code

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+
+provider "google" {
+  credentials = file("amiable-evening-413709-acef4404927f.json")
+  project     = "prwatech00J"
+  region      = "us"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name          = "tf-bucket"
+  location      = "US"
+  force_destroy = true
+  
+  uniform_bucket_level_access = true
+}
